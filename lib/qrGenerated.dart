35bc4d31069512fa21ae4197746d60940c771138
Pedro Arturo Higuera Garzón <higueragarzonpedroarturo@gmail.com>
2021-05-16 17:52:38 -0500

    few changes

--- lib/qrGenerated.dart
+++ lib/qrGenerated.dart
@@ -21,24 +21,29 @@ class _QrGenPageState extends State<QrGenPage> {
           iconTheme: IconThemeData(color: Colors.black),
           backgroundColor: Color(0xff3EC2C2)),
       body: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Container(
-            child: QrImage(data: valueQr),
-            width: 280,
-            height: 280,
-            decoration: BoxDecoration(
-                color: Colors.grey.shade200,
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 3,
-                    blurRadius: 10,
-                  )
-                ]),
+        child: Column(
+          
+          children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 25),
+            child: Container(
+              child: QrImage(data: valueQr),
+              width: 280,
+              height: 280,
+              decoration: BoxDecoration(
+                  color: Colors.grey.shade200,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 3,
+                      blurRadius: 10,
+                    )
+                  ]),
+            ),
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
             child: TextField(
               controller: urlController,
               decoration: InputDecoration(
@@ -47,29 +52,34 @@ class _QrGenPageState extends State<QrGenPage> {
                   color: Color(0xff3EC2C2),
                 ),
                 border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
                 focusedBorder: OutlineInputBorder(
                     borderSide: BorderSide(color: Color(0xff3EC2C2))),
                 hintText: 'Enter a URL',
               ),
             ),
           ),
-          MaterialButton(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5)),
-              color: Color(0xff3EC2C2),
-              child: Text('Generate'),
-              onPressed: () {
-                setState(() {
-                  valueQr = urlController.text.toString();
-                });
-              }),
-          MaterialButton(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5)),
-              color: Color(0xff3EC2C2),
-              child: Text('Share'),
-              onPressed: _generateImage)
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              MaterialButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  color: Color(0xff3EC2C2),
+                  child: Text('Generate'),
+                  onPressed: () {
+                    setState(() {
+                      valueQr = urlController.text.toString();
+                    });
+                  }),
+              MaterialButton(
+                minWidth: 50,
+                
+                child: Icon(Icons.share),
+                onPressed: () {},
+              )
+            ],
+          ),
         ]),
       ),
     );
