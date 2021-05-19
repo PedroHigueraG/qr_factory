afabfdc2c1bb44121864939addaf21160139ebc5
Pedro Arturo Higuera Garzón <higueragarzonpedroarturo@gmail.com>
2021-05-18 19:01:27 -0500

    Scrollbar added to qr code generator

--- lib/qrGenerated.dart
+++ lib/qrGenerated.dart
@@ -20,67 +20,66 @@ class _QrGenPageState extends State<QrGenPage> {
           title: Text('QR Code for URL', style: TextStyle(color: Colors.black)),
           iconTheme: IconThemeData(color: Colors.black),
           backgroundColor: Color(0xff3EC2C2)),
-      body: Center(
-        child: Column(
-          
-          children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 25),
-            child: Container(
-              child: QrImage(data: valueQr),
-              width: 280,
-              height: 280,
-              decoration: BoxDecoration(
-                  color: Colors.grey.shade200,
-                  borderRadius: BorderRadius.circular(10),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 3,
-                      blurRadius: 10,
-                    )
-                  ]),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-            child: TextField(
-              controller: urlController,
-              decoration: InputDecoration(
-                prefixIcon: Icon(
-                  Icons.link,
-                  color: Color(0xff3EC2C2),
-                ),
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
-                focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xff3EC2C2))),
-                hintText: 'Enter a URL',
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 25),
+              child: Container(
+                child: QrImage(data: valueQr),
+                width: 280,
+                height: 280,
+                decoration: BoxDecoration(
+                    color: Colors.grey.shade200,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 3,
+                        blurRadius: 10,
+                      )
+                    ]),
               ),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              MaterialButton(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                  color: Color(0xff3EC2C2),
-                  child: Text('Generate'),
-                  onPressed: () {
-                    setState(() {
-                      valueQr = urlController.text.toString();
-                    });
-                  }),
-              MaterialButton(
-                minWidth: 50,
-                
-                child: Icon(Icons.share),
-                onPressed: () {},
-              )
-            ],
-          ),
-        ]),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+              child: TextField(
+                controller: urlController,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.link,
+                    color: Color(0xff3EC2C2),
+                  ),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xff3EC2C2))),
+                  hintText: 'Enter a URL',
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MaterialButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    color: Color(0xff3EC2C2),
+                    child: Text('Generate'),
+                    onPressed: () {
+                      setState(() {
+                        valueQr = urlController.text.toString();
+                      });
+                    }),
+                MaterialButton(
+                  minWidth: 50,
+                  child: Icon(Icons.share),
+                  onPressed: () {},
+                )
+              ],
+            ),
+          ]),
+        ),
       ),
     );
   }
